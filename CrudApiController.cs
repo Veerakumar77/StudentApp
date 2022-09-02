bbdb7256a63b89a3c0cc36954144f9949f2517e9
Veerakumar77 <112682952+Veerakumar77@users.noreply.github.com>
2022-09-02 12:06:39 +0530

    Add files via upload

---
+++ CrudApiController.cs
@@ -0,0 +1,71 @@
+﻿using StudentRestApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StudentRestApi.Controllers
+{
+    public class CrudApiController : ApiController
+    {
+        crudEntities db = new crudEntities();
+
+        [HttpGet]
+        [Route ("api/crudApi/subject")]
+        public IHttpActionResult GetSubject()
+        {
+            List<subject> list = db.subjects.ToList();
+            return Ok(list);
+        }
+
+        [HttpGet]
+        [Route("api/crudApi/subject/{id}")]
+        public IHttpActionResult GetById(int id)
+        {
+            var x = db.subjects.Where(model => model.SubjectId == id).FirstOrDefault();
+            return Ok(x);
+        }
+
+        [HttpPost]
+        [Route("api/crudApi/subject")]
+        public IHttpActionResult create(subject model)
+        {
+            
+            db.subjects.Add(model);
+            db.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("api/crudApi/subject/{id}")]
+        public IHttpActionResult Edit(subject s)
+        {
+            var x = db.subjects.Where(model => model.SubjectId == s.SubjectId).FirstOrDefault();
+            if (x != null)
+            {
+                x.SubjectId = s.SubjectId;
+                x.Subjects = s.Subjects;
+                db.SaveChanges();
+            }
+            else
+            {
+                return NotFound();
+            }
+            return Ok();
+
+            
+        }
+
+        [HttpDelete]
+        [Route("api/crudApi/subject/{id}")]
+        public IHttpActionResult delete(int id)
+        {
+            var x = db.subjects.Where(model => model.SubjectId == id).FirstOrDefault();
+            db.Entry(x).State = System.Data.EntityState.Deleted;
+            db.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ StudentController.cs
@@ -0,0 +1,82 @@
+﻿using StudentRestApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StudentRestApi.Controllers
+{
+    public class StudentController : ApiController
+    {
+        crudEntities db = new crudEntities();
+
+        [HttpGet]
+        [Route("api/crudApi/student")]
+        public IHttpActionResult GetStudents()
+        {
+            List<student> list = db.students.ToList();
+            return Ok(list);
+        }
+
+        [HttpGet]
+        [Route("api/crudApi/student/{id}")]
+        public IHttpActionResult GetById(int id)
+        {
+            var x = db.students.Where(model => model.RegNo == id).FirstOrDefault();
+            return Ok(x);
+        }
+
+        [HttpPost]
+        [Route("api/crudApi/student")]
+        public HttpResponseMessage create(student model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (db.students.Any(x => x.StudentName == model.StudentName))
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, message: "StudentName is Already Exists");
+                }
+                else
+                {
+                    db.students.Add(model);
+                    db.SaveChanges(); 
+                }
+            }
+            return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            
+        }
+
+        [HttpPut]
+        [Route("api/crudApi/student/{id}")]
+        public IHttpActionResult Edit(student s)
+        {
+            var x = db.students.Where(model => model.RegNo == s.RegNo).FirstOrDefault();
+            if (x != null)
+            {
+                x.RegNo = s.RegNo;
+                x.StudentName = s.StudentName;
+                x.SubjectId = s.SubjectId;
+                db.SaveChanges();
+            }
+            else
+            {
+                return NotFound();
+            }
+            return Ok();
+
+
+        }
+
+        [HttpDelete]
+        [Route("api/crudApi/student/{id}")]
+        public IHttpActionResult delete(int id)
+        {
+            var x = db.students.Find(id);
+            db.students.Remove(x);
+            db.SaveChanges();
+            return Ok();
+        }
+    }
+}
